fd4adfb75bf0dcb22f6b45eb6877d4d8793c7f07
Nebiel Mohammed <nebielmohammedhotmail.com@Air-von-Nebiel.fritz.box>
2024-06-12 15:04:38 +0200

    added deleting feature

--- app.py
+++ app.py
@@ -34,5 +34,35 @@ def add():
     return render_template('add.html')
 
 
+@app.route('/delete/<int:post_id>', methods=["POST"])
+def delete(post_id):
+    with open("data.json", "r") as dataobj:
+        blog_posts = json.load(dataobj)
+    for post in blog_posts:
+        if post["id"] == post_id:
+            blog_posts.remove(post)
+            break
+    with open("data.json", "w") as dataobj:
+        json.dump(blog_posts, dataobj)
+    return render_template("index.html", posts=blog_posts)
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    # Fetch the blog posts from the JSON file
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+    # Update the post in the JSON file
+    # Redirect back to index
+
+    # Else, it's a GET request
+    # So display the update.html page
+    return render_template('update.html', post=post)
+
+
 if __name__ == '__main__':
     app.run()
